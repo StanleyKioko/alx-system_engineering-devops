b5bcc32454e52b40730761b6d521f1765b526703
StarnleyKioko <mwangestanley02@gmail.com>
2022-06-15 08:12:18 -0700

    6-main.h

---
+++ 0x06-pointers_arrays_strings/6-main.h
@@ -0,0 +1,23 @@
+#ifndef INV_TREE_H
+#define INV_TREE_H
+
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char str[] = "Expectthe best.Prepare for the worst.Capitalize on what comes.";
+
+	ptr = cap_string(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
+}
+#endif
+
